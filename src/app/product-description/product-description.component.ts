bf6e3603031722a5a88852239e48ee474422c5c4
Rajesh Kumar <Rajesh.Kumar@cox.com>
2018-09-19 15:14:21 +0530

    file changes

--- src/app/product-description/product-description.component.ts
+++ src/app/product-description/product-description.component.ts
@@ -1,7 +1,6 @@
-import { Album } from './../album';
 import { Component, OnInit } from '@angular/core';
 import { ProductService } from './../product.service';
-
+import { Album } from './../album';
 @Component({
   selector: 'app-product-description',
   templateUrl: './product-description.component.html',
