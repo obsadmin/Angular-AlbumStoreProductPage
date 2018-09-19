f12897a388e02aa110193e217b5e7da94424c2d9
Rajesh Kumar <Rajesh.Kumar@cox.com>
2018-09-19 15:29:22 +0530

    file changes

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,3 +1,4 @@
+import { Product } from './product';
 import { Observable } from 'rxjs/Observable';
 import { Album } from './album';
 import { Http, Response } from '@angular/http';
@@ -7,12 +8,17 @@ import 'rxjs/add/operator/map';
 @Injectable()
 export class ProductService {
   private _albumUrl:string = '../assets/album.json';
-
+  private _productsUrl:string = '../assets/products.json';
   constructor(private _http: Http) { }
   
   getAlbum(id:number) : Observable<Album>{
     return this._http.get(this._albumUrl).map(
       (response)=><Album>response.json()
     );
+  }
+  getProducts() : Observable<Product[]>{
+    return this._http.get(this._productsUrl).map(
+      (response)=><Product[]>response.json()
+    )
   }
 }
