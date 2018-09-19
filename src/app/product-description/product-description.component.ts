91ef984ea89516af1490a2ee56875b8748de551f
Rajesh Kumar <Rajesh.Kumar@cox.com>
2018-09-19 15:02:23 +0530

    file changes

--- src/app/product-description/product-description.component.ts
+++ src/app/product-description/product-description.component.ts
@@ -1,5 +1,7 @@
+import { Album } from './../album';
 import { Component, OnInit } from '@angular/core';
 import { ProductService } from './../product.service';
+
 @Component({
   selector: 'app-product-description',
   templateUrl: './product-description.component.html',
@@ -7,7 +9,7 @@ import { ProductService } from './../product.service';
 })
 export class ProductDescriptionComponent implements OnInit {
 
-  albumInfo;
+  private albumInfo:Album;
   constructor(private _productService:ProductService) { }
 
   ngOnInit() {
